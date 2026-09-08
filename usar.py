f567869127ac5adfc88cd3b142dcf86255663df0
Joao0Victor0do0Rosario0Dias <138147365+Joao0Victor0do0Rosario0Dias@users.noreply.github.com>
2026-09-08 17:09:43 -0300

    Update usar.py

--- usar.py
+++ usar.py
@@ -12,12 +12,12 @@ if gpus:
     except RuntimeError as e:
         print(e)
 classificador=Sequential()
-classificador.add(Conv2D(32,(3,3),input_shape=(64,64,3),activation='relu'))#recomendado 64 input_shape maior
-classificador.add(BatchNormalization())#deixa entre 0 e 1
-classificador.add(MaxPooling2D(pool_size=(2,2)))#matriz 4 pixels
+classificador.add(Conv2D(32,(3,3),input_shape=(64,64,3),activation='relu'))
+classificador.add(BatchNormalization())
+classificador.add(MaxPooling2D(pool_size=(2,2)))
 classificador.add(Conv2D(32,(3,3),activation='relu'))
-classificador.add(BatchNormalization())#deixa entre 0 e 1
-classificador.add(MaxPooling2D(pool_size=(2,2)))#matriz 4 pixels
+classificador.add(BatchNormalization())
+classificador.add(MaxPooling2D(pool_size=(2,2)))
 classificador.add(Flatten())
 classificador.add(Dense(units=128,activation='relu'))
 classificador.add(Dropout(0.2))
